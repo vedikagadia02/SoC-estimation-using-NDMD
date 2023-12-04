66f9f6d698338ed96efea413cb37b3e3b6d7147d
Aryaman <aryaman310@gmail.com>
2023-12-04 23:12:52 +0530

    jkmkjkmkjkmkjkmk

--- matlab/neural_network_koopman/KoopmanNetwork.m
+++ matlab/neural_network_koopman/KoopmanNetwork.m
@@ -2,15 +2,18 @@ classdef KoopmanNetwork < handle
     properties
         encoder
         decoder
+        autoencoder
         obsdim
         kMatrixDiag
         kMatrixUT
+        freezeKoopman
     end
     
     methods
-        function obj = KoopmanNetwork(indim, obsdim)
+        function obj = KoopmanNetwork(indim, obsdim,freezeKoopman)
             obj.obsdim = obsdim;
-            
+            obj.freezeKoopman = freezeKoopman;
+
             obj.encoder = [
                 fullyConnectedLayer(100)
                 reluLayer
@@ -30,15 +33,36 @@ classdef KoopmanNetwork < handle
                 reluLayer
                 fullyConnectedLayer(indim)
             ];
+            
 
-            obj.kMatrixDiag = rand(1, obsdim);
-            obj.kMatrixUT = 0.01 * randn(1, int32(obsdim*(obsdim-1)/2));
-
+            %obj.kMatrixDiag = rand(1, obsdim);
+            %obj.kMatrixUT = 0.01 * randn(1, int32(obsdim*(obsdim-1)/2));
+            
             obj.initializeWeights(obj.encoder);
             obj.initializeWeights(obj.decoder);
-
+            %obj.initializeWeights(obj.autoencoder);
+            obj.autoencoder = [obj.encoder,obj.decoder];
+            if obj.freezeKoopman
+                obj.freezeParameters();
+            end
             disp(['Total number of parameters: ', num2str(obj.numParameters())]);
         end
+        function freezeParameters(obj)
+            % Method to freeze parameters 
+            %obj.kMatrixDiag = obj.freezeParameter(obj.kMatrixDiag);
+            %obj.kMatrixUT = obj.freezeParameter(obj.kMatrixUT);
+            obj.autoencoder.layers(4) = obj.freezeParameter(obj.autoencoder.layers(4));
+        end
+        
+        function param = freezeParameter(obj, param)
+            % Helper method to freeze a specific parameter
+            if isprop(param, 'Learnable')
+                param.Learnable = false;
+            else
+                warning('Parameter does not have a Learnable property. Cannot freeze.');
+            end
+        end
+
 
         function weights = customWeightInitializer(obj, sz, meanValue, stdDeviation)
             weights = meanValue + stdDeviation * randn(sz);
@@ -56,8 +80,8 @@ classdef KoopmanNetwork < handle
         end
 
         function [g, x0] = forward(obj, x)
-            g = predict(obj.encoder, x);
-            x0 = predict(obj.decoder, g);
+            g = predict(obj.autoencoder.layers(1), x);
+            x0 = predict(obj.autoencoder.layers(2), g);
         end
 
         function x0 = recover(obj, g)
@@ -102,7 +126,5 @@ classdef KoopmanNetwork < handle
                 end
             end
         end
-
-        function backprop(obj, ae, input_t, output_t, )
     end
 end
--- matlab/neural_network_koopman/train_ae.m
+++ matlab/neural_network_koopman/train_ae.m
@@ -1,11 +1,15 @@
 function loss_ae = train_ae(training_input_data, indim, obsdim)
     loss_ae = 0;
-    ae = KoopmanNetwork(indim, obsdim); 
+    lr=1e-3;
+    ae = KoopmanNetwork(indim, obsdim,true)
+    net = dlnetwork([ae.encoder,ae.decoder]);
     for idx = 1:750399
         input_t = training_input_data(idx, :, :);
         encoder_output_t, output_t = ae.forward(training_input_data);
         loss_rec = immse(input_t, output_t);
-        
+        gradients = dlgradient(loss_rec,network.Learnables,RetainData=true);
+        net = adamupdate(net,gradients,lr);
         loss_ae = loss_ae + loss_rec;
+        
     end
 end
